85172e9efc6a86988f3c5bf6b73f48e9d810cfb4
FabioMatheus777 <fabiomatheusxlr8@gmail.com>
2022-05-03 18:37:24 -0300

    mudança no layout e começo de implementação de logica

--- pokedex/src/Hooks/UseRequestData.js
+++ pokedex/src/Hooks/UseRequestData.js
@@ -1,19 +1,20 @@
+import { useEffect, useState } from "react";
 import axios from "axios";
-import React, { useEffect } from "react";
-import { useState, useEffect } from "react";
 
-export const  UseRequestData = (url) => {
-    const [data, setData] = useState();
+export const UseRequestData = (url) => {
+  const [data, setData] = useState();
 
-    useEffect(()=>{
-        axios.get(url)
-        .then((response)=>{ 
-            setData(response.data)
-        })
-        .catch((error)=>{
-            console.log(error);
-            alert("Ocorreu um erro, tente novamente");
-        })
-    },[url])
-    return [data]
-}
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("Ocorreu um erro, tente novamente");
+      });
+  }, [url]);
+
+  return [data];
+};
--- pokedex/src/components/Card.js
+++ pokedex/src/components/Card.js
@@ -1,39 +1,64 @@
 import React from "react";
 import styled from 'styled-components'
+import { useNavigate } from "react-router-dom";
 
-const Container = styled.div`
-    width: 250px;
-    height: 300px;
-    border: solid black;
-    img{
-        border:solid red;
-        width: 100%;
-        height: 80%;
-    }
-    div{
+
+const DivGeral = styled.div`
+    height:250px;
+    width:200px;
+    padding: 10px;
+`
+const Fotinha = styled.img`
+    width:100%;
+    height:70%;
+    border-radius: 10px;
+`
+const DivButton = styled.div`
         display: flex;
-        justify-content: space-between;
-        border:solid green;
+        justify-content: space-evenly;
         width: 100%;
-        height: 20%;
-    }
-    button{
-        width: 50%;
+        height: 20%;   
+`   
+
+const ButtonAdd = styled.button`
+        width: 40%;
+        border-radius: 10px;
+        border: none;
         :hover{
             cursor: pointer;
-        }       
-    }
+        }
+`
+const DetalhesButton = styled.button`
+        width: 40%;
+        border-radius: 10px;
+        border: none;
+        :hover{
+            cursor: pointer;
+        }
 `
 
 export const Card = () => {
+    const navigate = useNavigate()
+
     
+
+    const irDetalhes = () => {
+        navigate('/Detalhes')
+    }
+
+
+
     return(     
-        <Container>
-            <img src="https://hiperideal.vteximg.com.br/arquivos/ids/167660-1000-1000/27502.jpg?v=636615816147030000" alt='banana' />
-            <div>
-                <button>Adicionar a Pokedex</button>
-                <button>Ver Detalhes</button>
-            </div>
-        </Container>       
+        <div>
+            <DivGeral>
+                <div>
+                    <Fotinha src="https://hiperideal.vteximg.com.br/arquivos/ids/167660-1000-1000/27502.jpg?v=636615816147030000" alt='banana' />
+                </div>
+                <DivButton>
+                    <ButtonAdd>Adicionar a Pokedex</ButtonAdd>
+                    <DetalhesButton className="Detalhes" onClick={irDetalhes}>Ver Detalhes</DetalhesButton>
+                </DivButton>
+            </DivGeral>
+        </div>       
     )
 }
