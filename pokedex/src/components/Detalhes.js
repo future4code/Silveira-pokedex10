11aba62d8da54e7591e9c8e168f0ba099bc0fb11
arthurpraado <arthurbmprado@gmail.com>
2022-05-06 17:26:21 -0300

    site quase pronto

--- pokedex/src/components/Detalhes.js
+++ pokedex/src/components/Detalhes.js
@@ -1,20 +1,38 @@
-import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import axios from "axios";
-import styled from 'styled-components'
+import styled from "styled-components";
 import { useRequestData } from "../Hooks/UseRequestData";
 
-const SessaoDado = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-    border: solid 2px black;
-    padding: 5px;
-    margin: 5px;
-    gap: 5px;
+const Botao = styled.button`
+  border-radius: 50%;
+  &:hover {
+    color: blue;
+    background-color: yellowgreen;
+    transition: linear 0.5s;
+    cursor: pointer;
+  }
 `
+
+const CorpoDetalhes = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 15px;
+  margin: 5vh 25vw;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  text-align: center;
+`
+
+const SessaoDado = styled.div`
+  display: flex;
+  padding: 15px;
+  margin: auto;
+  justify-content: center;
+  align-items: center;
+  align-self: center;
+  gap: 20px;
+  text-align: center;
+`;
 
 export const Detalhes = () => {
   const navigate = useNavigate();
@@ -26,27 +44,30 @@ export const Detalhes = () => {
     moves: [],
     types: [],
     sprites: {},
-  }
+  };
 
-  const [detalhes] = useRequestData(`https://pokeapi.co/api/v2/pokemon/${id}`, estadoInicial)
+  const [detalhes] = useRequestData(
+    `https://pokeapi.co/api/v2/pokemon/${id}`,
+    estadoInicial
+  );
 
   const goBack = () => {
     navigate(-1);
   };
 
-//   if (!detalhes ou detalhes === undefined) {
-//       return (
-//           <div>
-//               Carregando... (outra opção para a variável estadoInicial para nos proteger quando os dados da API não tiverem chego por erro ou demora)
-//           </div>
-//       )
-//   }
+  //   if (!detalhes ou detalhes === undefined) {
+  //       return (
+  //           <div>
+  //               Carregando... (outra opção para a variável estadoInicial para nos proteger quando os dados da API não tiverem chego por erro ou demora)
+  //           </div>
+  //       )
+  //   }
 
   const poderesPokemon = detalhes.stats.map((poder) => {
     return (
       <div>
+        <h4>{poder.stat.name}:</h4>
         <p>{poder.base_stat}</p>
-        <p>{poder.stat.name}</p>
       </div>
     );
   });
@@ -54,7 +75,7 @@ export const Detalhes = () => {
   const habilidadesPokemon = detalhes.abilities.map((habilidade) => {
     return (
       <div>
-        <p>{habilidade.ability.name}</p>
+        <h4> Habilidades do Pokemon: {habilidade.ability.name.toUpperCase()}</h4>
       </div>
     );
   });
@@ -62,7 +83,7 @@ export const Detalhes = () => {
   const principaisAtaques = detalhes.moves.slice(0, 3).map((ataque) => {
     return (
       <div>
-        <p> {ataque.move.name} </p>
+        <h4> Ataque principal: {ataque.move.name.toUpperCase()} </h4>
       </div>
     );
   });
@@ -70,23 +91,27 @@ export const Detalhes = () => {
   const tiposDoPokemon = detalhes.types.map((tipo) => {
     return (
       <div>
-        <p> {tipo.type.name} </p>
+        <h4> Tipo do Pokemon: {tipo.type.name.toUpperCase()} </h4>
       </div>
     );
   });
 
   return (
-    <div>
-      {detalhes.name}
+    <CorpoDetalhes>
+      <h1>
+        <strong>
+          <i>{detalhes.name}</i>
+        </strong>
+      </h1>
       <SessaoDado>
-        <img src={detalhes.sprites.back_default} alt="banana" />
-        <img src={detalhes.sprites.front_default} alt="batata" />
+        <img src={detalhes.sprites.back_default} alt="Pokemon de costas" />
+        <img src={detalhes.sprites.front_default} alt="Pokemon de frente" />
       </SessaoDado>
       <SessaoDado>{tiposDoPokemon}</SessaoDado>
       <SessaoDado>{principaisAtaques}</SessaoDado>
       <SessaoDado>{habilidadesPokemon}</SessaoDado>
       <SessaoDado>{poderesPokemon}</SessaoDado>
-      <button onClick={goBack}>voltar</button>
-    </div>
+      <Botao onClick={goBack}>Voltar</Botao>
+    </CorpoDetalhes>
   );
 };
--- pokedex/src/components/Pokedex.js
+++ pokedex/src/components/Pokedex.js
@@ -1,82 +1,107 @@
 import React, { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { GlobalContext } from "../contexts/GlobalContext";
-import { PokedexContext } from "../contexts/PokedexContext";
 import { getPokemons } from "../request/requests";
+import styled from "styled-components";
+
+const Titulo = styled.h1`
+  display: flex;
+  justify-content: center;
+  margin: 10px 500px;
+`;
+
+const Body = styled.div`
+  min-height: auto;
+`;
+
+const Botao = styled.button`
+  margin: 10px;
+  border-radius: 30%;
+
+  &:hover {
+    border-radius: 15%;
+    color: blue;
+    background-color: yellowgreen;
+    transition: linear 0.5s;
+    cursor: pointer;
+  }
+`;
+
+const PokemonItem = styled.div`
+  display: flex;
+  border: dashed 2px black;
+  margin: 20px 0;
+  gap: 10px;
+  align-items: center;
+  justify-content: center;
+  box-shadow: rgb(0 0 0 / 70%) 0px 8px 16px 0px;
+  button {
+    max-height: 20px;
+  }
+`;
 
 export const Pokedex = () => {
-    const navigate = useNavigate()
-    const [image , setImage] = useState("")
-    const { states, setters } = useContext(GlobalContext)
-    const { pokemonsPokedex, pokemonsHome } = states
-    const { setPokemons, setPokemonPokedex , setPokemonsHome } = setters
-    const [ imageContainer, setImageContainer ] = useState('')
+  const navigate = useNavigate();
+  const { states, setters } = useContext(GlobalContext);
+  const { pokemonsPokedex, pokemonsHome } = states;
+  const { setPokemons, setPokemonPokedex, setPokemonsHome } = setters;
 
-    const goBack = () => {
-        navigate(-1)
-    }
+  const goBack = () => {
+    navigate(-1);
+  };
 
-    const renderPokemonsList = pokemonsPokedex.map((pokemon) => {
-        return (
-            <div key={pokemon.name}>
-                <div onClick={() => onClickImage(pokemon)}>
-                    <img src={pokemon.sprites.versions["generation-viii"].icons.front_default} alt='Pokemon Icone'/>
-                    <p>{pokemon.name}</p>
-                </div>
-            </div>
-        )
-    })
+  const goToDetails = (id) => {
+    navigate(`/Detalhes/${id}`);
+  };
 
-    const removeFromPokedex = (pokemon) => {
-        const newPokemonsPokedex = [...pokemonsPokedex]
-        const index = pokemonsPokedex.findIndex(
-            (item) => item.name === pokemon.name);
-
-        newPokemonsPokedex.splice(index, 1)
-        setPokemonPokedex(newPokemonsPokedex)
-
-        const newPokemons = [...pokemonsHome, pokemon];
-        const orderedPokemons = newPokemons.sort((a, b) => 
-        {return a.id - b.id; })
-        setPokemonsHome(orderedPokemons)
-        setImageContainer('')
-    }
-
-    const CleanPokedex = () => {
-        setPokemonPokedex([])
-        getPokemons(setPokemons)
-        setImageContainer('')
-    }
-
-    const onClickImage = (pokemon) => {
-        setImageContainer(
-            <div>
-                <img src={pokemon.sprites.other.dream_world.front_default} alt={pokemon.name} />
-                    <button onClick={() => removeFromPokedex(pokemon)}>Remover</button>
-                    <button>Detalhes</button> 
-            </div>
-        )
-    }
-
+  const renderPokemonsList = pokemonsPokedex.map((pokemon) => {
     return (
-        <PokedexContext.Provider value={{image , setImage}}>
+      <PokemonItem key={pokemon.name}>
+        <img
+          src={pokemon.sprites.versions["generation-viii"].icons.front_default}
+          alt="Pokemon Icone"
+        />
+        <h4>{pokemon.name.toUpperCase()}</h4>
+        <Botao onClick={() => removeFromPokedex(pokemon)}>Remover</Botao>
+        <Botao onClick={() => goToDetails(pokemon.name)}>Detalhes</Botao>
+      </PokemonItem>
+    );
+  });
+
+  const removeFromPokedex = (pokemon) => {
+    const newPokemonsPokedex = [...pokemonsPokedex];
+    const index = pokemonsPokedex.findIndex(
+      (item) => item.name === pokemon.name
+    );
+
+    newPokemonsPokedex.splice(index, 1);
+    setPokemonPokedex(newPokemonsPokedex);
+
+    const newPokemons = [...pokemonsHome, pokemon];
+    const orderedPokemons = newPokemons.sort((a, b) => {
+      return a.id - b.id;
+    });
+    setPokemonsHome(orderedPokemons);
+  };
+
+  const CleanPokedex = () => {
+    setPokemonPokedex([]);
+    getPokemons(setPokemons);
+  };
+
+  return (
+    <>
+      <div></div>
+      <div>
         <div>
-            <button onClick={goBack}>Voltar</button>
-            <h2>Pokedex</h2>
+          <Titulo>Pokedex</Titulo>
+          <Botao onClick={CleanPokedex}>Limpar Tela</Botao>
         </div>
         <div>
-            <div>
-                <button onClick={CleanPokedex}>Limpar Tela</button>
-            </div>
-            <div>
-                <div>
-                    {imageContainer}
-                </div>
-                <div>
-                    {renderPokemonsList}
-                </div>
-            </div>
+          <div>{renderPokemonsList}</div>
+          <Botao onClick={goBack}>Voltar</Botao>
         </div>
-    </PokedexContext.Provider>
-    )
-}
+      </div>
+    </>
+  );
+};
